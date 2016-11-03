f3b5261f17b9a1fbcde7368aead877525b4f0315
Sharif Malik <sharif.malik@neptune-int.com>
2016-11-03 16:47:04 +0530

    added route middleware to authenticate and check token

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.use(require('./userController'));
+// ---------------------------------------------------------
+// authenticated routes
+// ---------------------------------------------------------
 router.use(require('./fileController'));
 module.exports = router;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,44 @@
 // express
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
+var config = require('../config/config');
 var userBusinessService = require('../business/userBusinessService');
 
 router.post('/authenticate', function(req, res) {
     console.log("Authenticating User");
     return userBusinessService.authenticate(req, res);
+});
+
+// ---------------------------------------------------------
+// route middleware to authenticate and check token
+// ---------------------------------------------------------
+router.use(function(req, res, next) {
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+    // decode token
+    if (token) {
+        // verifies secret and checks exp
+        jwt.verify(token, config.secret, function(err, decoded) {
+            if (err) {
+                return res.status(403).json({
+                    success: false,
+                    message: 'Failed to authenticate token.'
+                });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+    }
 });
 
 module.exports = router;
